a788c9d354024df011a3d91cd471062781a7315a
mizrael <mizrael@yahoo.it>
2017-10-20 23:31:52 +0100

    removed hardcoded socket address

--- src/web/static/js/site.js
+++ src/web/static/js/site.js
@@ -1,8 +1,9 @@
 barfer.init = function (options) {
     console.log("starting barfer web client...");
 
+    var socketAddress = window.location.protocol + "//" + window.location.host;
     barfer.context = {
-        socket: io.connect('http://localhost:3100'),
+        socket: io.connect(socketAddress),
         user: options.user
     };
 
