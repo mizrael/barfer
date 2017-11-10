258a78c4af64e2eb5d5ddcc2aff605c2924e7935
mizrael <mizrael@yahoo.it>
2017-11-10 01:49:22 +0000

    updated deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -121,13 +121,13 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd - > /dev/null
 fi
 
-# 4. Run Gulp Build Task
-if [ -e "$DEPLOYMENT_TARGET/gulpfile.js" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  eval ./node_modules/.bin/gulp build
-  exitWithMessageOnError "gulp failed"
-  cd - > /dev/null
-fi
+# # 4. Run Gulp Build Task
+# if [ -e "$DEPLOYMENT_TARGET/gulpfile.js" ]; then
+#   cd "$DEPLOYMENT_TARGET"
+#   eval ./node_modules/.bin/gulp build
+#   exitWithMessageOnError "gulp failed"
+#   cd - > /dev/null
+# fi
 
 ##################################################################################################################################
 echo "Finished successfully."
