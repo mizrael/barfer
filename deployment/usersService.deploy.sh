29e5122780cadb22cf3beda03e49798e63cd9b26
mizrael <mizrael@yahoo.it>
2017-11-10 03:36:31 +0000

    moved build stage to every deployment

--- deployment/usersService.deploy.sh
+++ deployment/usersService.deploy.sh
@@ -1,3 +1,9 @@
+gulp
+rm -rf ./node_modules
+rm -rf ./tests
+rm -rf ./src
+rm -rf ./bin/fakeDataFeeder
+rm -rf ./bin/messageProcessor
 rm -rf ./bin/barfsService
 rm -rf ./bin/web
 rm ./package.json
--- deployment/web.deploy.sh
+++ deployment/web.deploy.sh
@@ -1,2 +1,8 @@
+gulp
+rm -rf ./node_modules
+rm -rf ./tests
+rm -rf ./src
+rm -rf ./bin/fakeDataFeeder
+rm -rf ./bin/messageProcessor
 rm -rf ./bin/usersService
 rm -rf ./bin/barfsService
