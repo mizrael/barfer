e9ad75e9b770bda46d6b763ef7a26cf8137e5086
mizrael <mizrael@yahoo.it>
2017-11-11 09:36:47 +0000

    added message processor to the deployment pipeline

---
+++ deployment/messageProcessor.deploy.sh
@@ -0,0 +1,10 @@
+gulp
+rm -rf ./node_modules
+rm -rf ./tests
+rm -rf ./src
+rm -rf ./bin/fakeDataFeeder
+rm -rf ./bin/barfsService
+rm -rf ./bin/usersService
+rm -rf ./bin/web
+rm ./package.json
+mv ./deployment/package.messageProcessor.json ./package.json
