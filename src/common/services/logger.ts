7abeb76f21e77d59f9c5ced585f7626a462ca047
mizrael <mizrael@yahoo.it>
2017-11-12 00:09:00 +0000

    small update

--- src/common/services/logger.ts
+++ src/common/services/logger.ts
@@ -1,4 +1,13 @@
+import * as path from 'path';
+import * as fs from 'fs';
 import * as winston from 'winston';
+
+const logDir = './logs',
+    logFullPath = path.join(logDir, 'log.txt');
+
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
 
 winston.configure({
     level: 'verbose',
