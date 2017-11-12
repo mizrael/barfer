1fa5e14c0f89ceac282d701b27ed0bbc3922ce94
mizrael <mizrael@yahoo.it>
2017-11-12 14:17:55 +0000

    updated deployment script

--- deployment/messageProcessor.deploy.sh
+++ deployment/messageProcessor.deploy.sh
@@ -7,4 +7,5 @@ rm -rf ./bin/barfsService
 rm -rf ./bin/usersService
 rm -rf ./bin/web
 rm ./package.json
+mv ./bin/messageProcessor/app_data ./app_data
 mv ./deployment/package.messageProcessor.json ./package.json
