c22347d948df40a7d3b7ea91d6f503792eb07f20
mizrael <mizrael@yahoo.it>
2017-11-17 17:01:40 +0000

    added on the frontend added xss check on barfs service

--- src/barfsService/commands/createBarf.ts
+++ src/barfsService/commands/createBarf.ts
@@ -3,6 +3,13 @@ import { IPublisher } from "../../common/services/publisher";
 import { Message } from "../../common/services/message";
 import { Events, Exchanges } from "../../common/events";
 import * as logger from '../../common/services/logger';
+import * as xss from 'xss';
+
+interface CreateBarfDto {
+    id: string;
+    text: string;
+    authorId;
+}
 
 export class CreateBarf implements ICommand {
     constructor(public readonly id: string,
@@ -14,9 +21,12 @@ export class CreateBarfCommandHandler implements ICommandHandler<CreateBarf>{
     constructor(private readonly publisher: IPublisher) { }
 
     handle(command: CreateBarf): Promise<void> {
-        const task = new Message(Exchanges.Barfs, Events.BarfCreated, command);
+        const dto = command as CreateBarfDto;
+        dto.text = xss(dto.text);
+
+        const task = new Message(Exchanges.Barfs, Events.BarfCreated, dto);
         this.publisher.publish(task);
-        logger.info("published barf create command: " + JSON.stringify(command));
+        logger.info("published barf create command: " + JSON.stringify(dto));
         return Promise.resolve();
     }
 
