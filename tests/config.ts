e3ba7f41c930c7dcf8d8ea6aa0a695d8ea2565b8
mizrael <mizrael@yahoo.it>
2017-11-30 21:39:39 +0100

    updated connection string

--- tests/config.ts
+++ tests/config.ts
@@ -1,3 +1,3 @@
 export class IntegrationTestsConfig {
-    public static readonly mongoConnectionString: string = "mongodb://barfer:fRYXFCmd8IF6KhVw@cluster0-shard-00-00-yuik9.mongodb.net:27017,cluster0-shard-00-01-yuik9.mongodb.net:27017,cluster0-shard-00-02-yuik9.mongodb.net:27017/barferTests?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+    public static readonly mongoConnectionString: string = "mongodb://barfer:WG8fFEsNmQGN6JSk@cluster0-shard-00-00-rg322.mongodb.net:27017,cluster0-shard-00-01-rg322.mongodb.net:27017,cluster0-shard-00-02-rg322.mongodb.net:27017/barferTests?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 }
